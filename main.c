c51a91cda16a559462628423689cf0ac3885823e
deh5850 <78886396+deh5850@users.noreply.github.com>
2021-02-25 12:20:19 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -19,6 +19,10 @@ int scanner(char* buffer, char* errorBuffer, FILE* in_file, FILE* out_file, FILE
 //Given enum value of token, and a string, returns string value of token
 char * getTokenType(int token, char * str);
 
+// Checks if the string stored in buffer matches any of the hardcoded reserved names
+// If it matches a reserved name, then the corresponding token enumeration is returned, else -1 is returned
+int check_reserved(const char* buffer);
+
 // Gets parameters from the command prompt (if they exist) and stores them in "inputFilePath" and "outputFilePath" respectively
 void getCmdParameters(int argc, char** argv, char* inputFilePath, char* outputFilePath);
 
@@ -89,8 +93,8 @@ int main(int argc, char** argv)
 
 			//Note: Output file writing has been moved to the end of the scanner function
     	}
-    	
-    	printf("\n\nFinished Scanning: %s\n", inputFilePath);
+
+		printf("\n\nFinished Scanning: %s\n", inputFilePath);
 
 		sprintf(tokenBuffer, "\n\nThere are %d lexical errors.", errorNum); // Resuses the token buffer temporarily because why not
 		fputs(tokenBuffer, listFilePtr); // Puts the total number of errors at the end of the list file
@@ -258,6 +262,7 @@ int scanner(char* buffer, char* errorBuffer, FILE* in_file, FILE* out_file, FILE
 
 			c = '\0'; // So that the listing file doesn't write before a possible new line character
 			fseek(in_file, lastPos, SEEK_SET); // Puts the file pointer back a byte so that it doesn't skip the next character
+
 		}
 		else
 		{
@@ -350,64 +355,70 @@ int scanner(char* buffer, char* errorBuffer, FILE* in_file, FILE* out_file, FILE
 			}
 		}
 
-		// --------------------START OF LISTING FILE WRITING----------------------
-
-		//Write "buffer" to listing file ("buffer" now gets every character copied into to and emptied before the next token)
-		if (fputs(buffer, list_file) != 0)
+		if (token == SCANEOF)
 		{
-			printf("\nERROR writing to listing file...");
+			buffer = "EOF";
 		}
-
-		// Write the error into a buffer for later writing to the list file
-		if (token == ERROR)
+		else
 		{
+			// --------------------START OF LISTING FILE WRITING----------------------
 
-			(*errorNum)++;
-			strncat(errorBuffer, "Unexpected token '", ERROR_BUFFER_SIZE - strlen(errorBuffer));
-			strncat(errorBuffer, buffer, ERROR_BUFFER_SIZE - strlen(errorBuffer));
-			strncat(errorBuffer, "'\n", ERROR_BUFFER_SIZE - strlen(errorBuffer));
-			if (strlen(errorBuffer) >= ERROR_BUFFER_SIZE)
-				strcpy(errorBuffer, "Many unexpected tokens were found!"); // For cases in which the error buffer is too small
-
-		}
-	
-		//If reached end of line, print possible errors and new line number to listing file
-		if (c == '\n')
-		{
-
-			fputs(errorBuffer, list_file);
-			errorBuffer[0] = '\0';
-
-			(*lineNum)++;
-
-			int tempSize = (sizeof(char) * 3 + sizeof(char) * (log(*lineNum) / log(10) + 1));
-			// Allocates just enough space for the line number, a period, and a tab (and a NULL terminator) in the form of a character string
-			char* temp = malloc(tempSize);
-
-			//Converts the line number to a string and stores it in "listingBuffer"
-			itoa(*lineNum, temp, 10);
-			//Puts the period and tab after the line number
-			strcat(temp, ".\t");
-			
-			temp[tempSize / sizeof(char) - 1] = '\0'; // NULL terminates the string
-
-			// Writes the line number to the listing file
-			if (fputs(temp, list_file) != 0)
+			//Write "buffer" to listing file ("buffer" now gets every character copied into to and emptied before the next token)
+			if (fputs(buffer, list_file) != 0)
 			{
 				printf("\nERROR writing to listing file...");
 			}
-			
-			free(temp);
 
+			// Write the error into a buffer for later writing to the list file
+			if (token == ERROR)
+			{
+
+				(*errorNum)++;
+				strncat(errorBuffer, "Unexpected token '", ERROR_BUFFER_SIZE - strlen(errorBuffer));
+				strncat(errorBuffer, buffer, ERROR_BUFFER_SIZE - strlen(errorBuffer));
+				strncat(errorBuffer, "'\n", ERROR_BUFFER_SIZE - strlen(errorBuffer));
+				if (strlen(errorBuffer) >= ERROR_BUFFER_SIZE)
+					strcpy(errorBuffer, "Many unexpected tokens were found!"); // For cases in which the error buffer is too small
+
+			}
+
+			//If reached end of line, print possible errors and new line number to listing file
+			if (c == '\n')
+			{
+
+				fputs(errorBuffer, list_file);
+				errorBuffer[0] = '\0';
+
+				(*lineNum)++;
+
+				int tempSize = (sizeof(char) * 3 + sizeof(char) * (log(*lineNum) / log(10) + 1));
+				// Allocates just enough space for the line number, a period, and a tab (and a NULL terminator) in the form of a character string
+				char* temp = malloc(tempSize);
+
+				//Converts the line number to a string and stores it in "listingBuffer"
+				itoa(*lineNum, temp, 10);
+				//Puts the period and tab after the line number
+				strcat(temp, ".\t");
+
+				temp[tempSize / sizeof(char) - 1] = '\0'; // NULL terminates the string
+
+				// Writes the line number to the listing file
+				if (fputs(temp, list_file) != 0)
+				{
+					printf("\nERROR writing to listing file...");
+				}
+
+				free(temp);
+
+			}
+			// --------------------END OF LISTING FILE WRITING----------------------
 		}
-		// --------------------END OF LISTING FILE WRITING----------------------
-			
-		if (c == EOF)
-			token = SCANEOF;
+		
 		if (token != -1)
 		{
 
 			// --------------------START OF OUTPUT FILE WRITING----------------------
+
 			char tokenType[20] = { '\0' };
 			char temp[12];
 			char outputBuffer[70] = { '\0' }; //To print to output file
@@ -423,12 +434,12 @@ int scanner(char* buffer, char* errorBuffer, FILE* in_file, FILE* out_file, FILE
 			strcat(outputBuffer, temp);
 
 			//Add tab and "token type: " to output buffer
-			strcat(outputBuffer, "\t");
+			strcat(outputBuffer, "\t\t");
 			strcat(outputBuffer, "token type: ");
 			strcat(outputBuffer, tokenType);
 
 			//Add two tabs, "actual token: ", and actual token to output buffer
-			strcat(outputBuffer, "\t\t");
+			strcat(outputBuffer, "\t\t\t\t");
 			strcat(outputBuffer, "actual token: ");
 			strcat(outputBuffer, buffer); //actual token will be in token buffer
 			strcat(outputBuffer, "\n");
@@ -438,6 +449,7 @@ int scanner(char* buffer, char* errorBuffer, FILE* in_file, FILE* out_file, FILE
 			{
 				printf("\nERROR writing to output file...");
 			}
+
 			// --------------------END OF OUTPUT FILE WRITING----------------------
 
 			return token;
@@ -628,4 +640,3 @@ char * getTokenType(int token, char * str)
 			
 	return str;
 }
-
